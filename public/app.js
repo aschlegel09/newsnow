6c052e6694f870f0f2c4b57e304939ad06de404c
Andrew Schlegel <aschlegel09@gmail.com>
2018-12-11 10:02:58 -0500

    working front end

--- public/app.js
+++ public/app.js
@@ -1,88 +1,90 @@
 // We'll be rewriting the table's data frequently, so let's make our code more DRY
 // by writing a function that takes in 'art' (JSON) and creates a table body
-function displayResults(artTitles) {
-    // First, empty the table
-    $("tbody").empty();
+$(function () {
+    function displayResults(artTitles) {
+        // First, empty the table
+        $("tbody").empty();
 
-    // Then, for each entry of that json...
-    artTitles.forEach(function (art) {
-        // Append each of the art piece's properties to the table
-        var tr = $("<tr>").append(
-            $("<td>").text(art.title),
-            $("<td>").text(art.category),
-            $("<td>").text(art.link),
-            $("<td>").text(art.time),
-            $("<td>").text(art.image)
-        );
+        // Then, for each entry of that json...
+        artTitles.forEach(function (art) {
+            // Append each of the art piece's properties to the table
+            var tr = $("<tr>").append(
+                $("<td>").text(art.title),
+                $("<td>").text(art.category),
+                $("<td>").text(art.link),
+                $("<td>").text(art.time),
+                $("<td>").text(art.image)
+            );
 
-        $("tbody").append(tr);
-    });
-}
-
-// Bonus function to change "active" header
-function setActive(selector) {
-    // remove and apply 'active' class to distinguish which column we sorted by
-    $("th").removeClass("active");
-    $(selector).addClass("active");
-}
-
-function setWidth(selector) {
-    $("td").removeClass("active");
-    $(selector).addClass("td-width");
-}
-// 1: On Load
-// ==========
-
-// First thing: ask the back end for json with all artTitles
-$.getJSON("/all", function (data) {
-    // Call our function to generate a table body
-    // displayResults(data);
-       // Display the apropos information on the page
-       $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+            $("tbody").append(tr);
+        });
     }
-});
 
-// 2: Button Interactions
-// ======================
+    // Bonus function to change "active" header
+    function setActive(selector) {
+        // remove and apply 'active' class to distinguish which column we sorted by
+        $("th").removeClass("active");
+        $(selector).addClass("active");
+    }
 
-// When user clicks the title sort button, display table sorted by title
-$("#title-sort").on("click", function () {
-    // Set new column as currently-sorted (active)
-    setActive("#title");
+    function setWidth(selector) {
+        $("td").removeClass("active");
+        $(selector).addClass("td-width");
+    }
+    // 1: On Load
+    // ==========
 
-    // Do an api call to the back end for json with all artTitles sorted by title
-    $.getJSON("/title", function (data) {
+    // First thing: ask the back end for json with all artTitles
+    $.getJSON("/all", function (data) {
         // Call our function to generate a table body
-        displayResults(data);
-    });
+        // displayResults(data);
+        // Display the apropos information on the page
+        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    },
+
+
+        // 2: Button Interactions
+        // ======================
+
+        // When user clicks the title sort button, display table sorted by title
+        $("#title-sort").on("click", function () {
+            // Set new column as currently-sorted (active)
+            setActive("#title");
+
+            // Do an api call to the back end for json with all artTitles sorted by title
+            $.getJSON("/title", function (data) {
+                // Call our function to generate a table body
+                displayResults(data);
+            });
+        }),
+
+        // When user clicks the category sort button, display the table sorted by category
+        $("#category-sort").on("click", function () {
+            // Set new column as currently-sorted (active)
+            setActive("#category");
+
+            // Do an api call to the back end for json with all artTitles sorted by category
+            $.getJSON("/category", function (data) {
+                // Call our function to generate a table body
+                displayResults(data);
+            });
+        }),
+
+        //   scrape buttons
+        // clear the body of articles
+        $("#clear").on("click", function () {
+            event.preventDefault();
+            $("tbody").empty();
+        }),
+
+        //   redirect to scraper on click
+        $("#scrape").on("click", function () {
+            event.preventDefault();
+
+            // Run an AJAX request for any unsaved headlines
+            $.get("/scrape").then(function (data) {
+                displayResults(data);
+            });
+        })
+    );
 });
-
-// When user clicks the category sort button, display the table sorted by category
-$("#category-sort").on("click", function () {
-    // Set new column as currently-sorted (active)
-    setActive("#category");
-
-    // Do an api call to the back end for json with all artTitles sorted by category
-    $.getJSON("/category", function (data) {
-        // Call our function to generate a table body
-        displayResults(data);
-    });
-});
-
-//   scrape buttons
-// clear the body of articles
-$("#clear").on("click", function () {
-    event.preventDefault();
-    $("tbody").empty();
-});
-
-//   redirect to scraper on click
-$("#scrape").on("click", function () {
-    event.preventDefault();
-
-    // Run an AJAX request for any unsaved headlines
-    $.get("/scrape").then(function (data) {
-        displayResults(data);
-    });
-});
-
--- server.js
+++ server.js
@@ -15,18 +15,22 @@ var db = require("./models");
 // Initialize Express
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + "/public"));
+  // index route loads view.html
+  app.get("/", function (req, res) {
+    res.render('index');
+});
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
@@ -49,67 +53,67 @@ app.get("/scrape", function (req, res) {
     var $ = cheerio.load(response.data);
 
     // if (response.ObjectId) {
-      $(".teaser-info").each(function (i, element) {
-    // Save an empty result object
-    var result = {};
+    $(".teaser-info").each(function (i, element) {
+      // Save an empty result object
+      var result = {};
 
-    // Add the text and href of every link, and save them as properties of the result object
-    result.time = $(this).children(".teaser-blurb").text(); //teaser-blurb
-    result.title = $(this).children("a").text();
-    result.link = $(this).children("a").attr("href");
+      // Add the text and href of every link, and save them as properties of the result object
+      result.time = $(this).children(".teaser-blurb").text(); //teaser-blurb
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
 
-    // Create a new Article using the `result` object built from scraping
-    db.Article.create(result)
-      .then(function (dbArticle) {
-        // View the added result in the console
-        console.log(dbArticle);
-      })
-      .catch(function (err) {
-        // If an error occurred, send it to the client
-        return res.json(err);
-      });
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
+    });
   });
-});
   res.send("Scrape Complete");
   // res.redirect("/");
 });
 
 // Route for getting all Articles from the db
-app.get("/all", function(req, res) {
+app.get("/all", function (req, res) {
   // Grab every document in the Articles collection
   db.Article.find({})
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.json(err);
     });
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
+app.get("/articles/:id", function (req, res) {
 
   db.Article.findOne({ _id: req.params.id })
     .populate("note")
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.json(err);
     });
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id", function (req, res) {
 
   db.Note.create(req.body)
-    .then(function(dbNote) {
+    .then(function (dbNote) {
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.json(err);
     });
 });
