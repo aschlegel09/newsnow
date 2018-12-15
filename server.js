e2b5eaf4a1579e9bb6fe83efc281f772365e6b7c
Andrew Schlegel <aschlegel09@gmail.com>
2018-12-15 14:48:57 -0500

    fixed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var mongoose = require("mongoose");
 var logger = require("morgan");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Require axios and cheerio. This makes the scraping possible
 var axios = require("axios");
